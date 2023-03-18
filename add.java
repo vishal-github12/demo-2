dd917146b831c325c139bb2735ab722678d5a16c
vishal <vishal@gmail.com>
2023-03-19 00:10:26 +0530

    git task-1 file

--- add.java
+++ add.java
@@ -1,3 +1,13 @@
 hi 
 this is add.java module
 end
+
+
+public class LongExample1.
+{
+public static void main(String...k)
+{
+long num1=10L;
+long num2=-10L;
+System.out.println("num1: "+num1);
+System.out.println("num2: "+num2);
